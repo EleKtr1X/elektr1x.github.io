2e2aaa67912daba48e1cb413b47a8202d34c6016
EleKtr1X <aarnav.thite@gmail.com>
2023-07-02 15:59:18 -0400

    easter egg code cleanup

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ window.addEventListener("keydown", e => {
     const ps = document.getElementsByTagName('p');
     const imgs = document.getElementsByTagName('img');
     const icons = document.getElementsByTagName('i');
+    let l = icons.length;
 
-    console.log('works');
-    document.body.style.background = 'linear-gradient(to right, rgb(34, 215, 29), rgb(253, 46, 246), rgb(155, 83, 0)) 0% 50% / 400% 400%';
+    document.body.style.background = getComputedStyle(document.body).background.replace(/linear-gradient\(.+\)/, 'linear-gradient(to right, rgb(34, 215, 29), rgb(253, 46, 246), rgb(155, 83, 0))');
     document.getElementsByClassName('big-text')[0].textContent = 'Unpleasant Gradient';
 
-    h3s[0].textContent = 'Doors it\'s showed up at';
+    h3s[0].textContent = 'Front doors it\'s showed up at';
     for (let i = 1; i < h3s.length; i++) {
       h3s[i].textContent = 'Unpleasant Gradient';
       ps[i - 1].textContent = 'This unpleasant gradient shows up in your website';
@@ -21,11 +21,8 @@ window.addEventListener("keydown", e => {
     for (let i of imgs) {
       i.src = 'images/upg.png';
     }
-    let l = icons.length;
 
     for (let i = 0; i < l; i++) {
-      console.log(icons.length);
-      console.log(icons[0].outerHTML)
       icons[0].outerHTML = '<img src="images/upg.png"/>'
     }
   }
